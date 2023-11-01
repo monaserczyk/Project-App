3eff7df1720a9286234968f4c9f3ba1f8eb2642f
monaserczyk <serczykm@gmail.com>
2023-11-01 15:26:18 +0100

    dzien 1

---
+++ Project-App/Project-App/Dog.cs
@@ -0,0 +1,13 @@
+﻿namespace Project_App
+{
+    public class Dog
+    {
+        public Dog(string name, string age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+        public string Name { get; set; }
+        public string Age { get; set; }
+    }
+}
---
+++ Project-App/Project-App/Program.cs
@@ -0,0 +1,10 @@
+﻿using Project_App;
+
+Console.WriteLine("Welcome to Woof ya doin? The app to track your dogs' activity.");
+Console.WriteLine("What's your dog's name?");
+
+var dogsname = Console.ReadLine();
+Console.WriteLine("How old is your dog?");
+var dogsage = Console.ReadLine();
+var dog = new Dog(dogsname, dogsage);
+Console.WriteLine($"Hello {dog.Name}, nice to meet you !");
